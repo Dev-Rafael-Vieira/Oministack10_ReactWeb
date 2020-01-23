039da81d8f92add72104f7a088b4f53be132083a
Dev-Rafael_Vieira <rafael_egqsociesc@hotmail.com>
2020-01-23 01:30:01 -0300

    Primeira versão estática da aplicação

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import { async } from 'q';
+
+
+function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect( () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude} = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, [] )
+
+  async function handleAddDev(e){
+    e.preventDefault();
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block" >
+            <label htmlFor="github_username">Usuario do Github</label>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-block" >
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+
+            <div className="input-block" >
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)} //seta o valor do input dentro do valor latitude
+              />
+            </div>
+
+            <div className="input-block" >
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+               type="number" 
+               name="longitude" 
+               id="longitude" 
+               required 
+               value={longitude}
+               onChange={e => setLongitude(e.target.value)} //seta o valor do input dentro do valor longitude
+              />
+            </div>
+
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://www.petshopauqmia.com.br/wp-content/uploads/2009/11/gato_filhote.jpg" alt="gatinho"/>
+              <div className="user-info">
+                <strong>Gato da Net</strong>
+                <span>ReactJS, React Native, NodeJs</span>
+              </div>
+            </header>
+            <p>CTO na Rocketseat</p>
+            <a href="https://github.com/Dev-Rafael-Vieira">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
